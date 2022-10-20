4a097d2315c71c66412c67beb912108023e1cc58
Jamal Hussain <jamal@flip.space>
2022-10-20 18:37:58 +0400

    added products component

---
+++ components/Products/index.js
@@ -0,0 +1,30 @@
+import Image from "next/image"
+
+const Product = ({ image, title }) => {
+  return (
+    <div class="grid justify-items-center py-2 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-700">
+      <a href="#">
+        <Image
+          class="rounded-lg"
+          src={image}
+          alt=""
+          width="200px"
+          height="200px"
+        />
+      </a>
+      <div class="p-5">
+        <a href="#">
+          <h5 class="mb-2 text-md font-bold tracking-tight text-gray-900 dark:text-white">
+            {title}
+          </h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so
+          far, in reverse chronological order.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Product
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
 import Head from "next/head"
 import Image from "next/image"
+import { useEffect, useState } from "react"
 import styles from "../styles/Home.module.css"
+import Product from "../components/Products"
 
 export default function Home() {
-  return <div className=""> Initial</div>
+  const [productList, setProductList] = useState([])
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setProductList(json))
+  }, [])
+
+  return (
+    <div>
+      <div className="hidden md:block">Select number of tiles</div>
+      <div className="grid grid-cols-2 md:grid-cols-6 gap-1 p-4">
+        {productList.map((product) => (
+          <Product
+            key={product.id}
+            image={product.image}
+            title={product.title}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
