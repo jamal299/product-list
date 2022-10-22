216271ba178909a80bd11644f547e3b0afea67be
Jamal Hussain <jamal@flip.space>
2022-10-22 08:53:20 +0400

    removed a few commented lines

--- pages/index.js
+++ pages/index.js
@@ -112,13 +112,9 @@ export default function Home({ products }) {
 }
 
 export async function getStaticProps() {
-  // Call an external API endpoint to get posts.
-  // You can use any data fetching library
   const res = await fetch("https://fakestoreapi.com/products")
   const products = await res.json()
 
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
   return {
     props: {
       products,
